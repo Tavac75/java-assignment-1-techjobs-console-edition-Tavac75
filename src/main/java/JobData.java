4b04eb620782d97e1f481634bdaa1b54376d8e7f
Tavac75 <74030355+Tavac75@users.noreply.github.com>
2022-01-25 06:22:39 -0600

    Search Engine fixed

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -97,10 +97,22 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs){
+            for (String key : row.keySet()){
+                String aValue = row.get(key);
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
+                }
+            }
+        }
+        return jobs;
+    }
+
+
 
         // TODO - implement this method
-        return null;
-    }
 
     /**
      * Read in data from a CSV file and store it in a list
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,29 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        if (someJobs.size() == 0) {
+            System.out.print("No Results");
+            return;
+        }
 
-        System.out.println("printJobs is not implemented yet");
+        for (HashMap<String, String> job : someJobs) {
+
+            String jobInfo = "\n\n";
+
+            for (Map.Entry<String, String> jobColumn: job.entrySet()) {
+                jobInfo += (jobColumn.getKey() + ": " + jobColumn.getValue() + "\n");
+            }
+
+            jobInfo += "";
+
+            System.out.println(jobInfo);
+        }
     }
 }
